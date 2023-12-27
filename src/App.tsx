9ccfad5540611d11d199d1aae2bfaa87ee6020d6
Tohir Kuliev <kuliev.tech@gmail.com>
2023-12-27 12:30:57 -0500

    Add onChange and value to Input component

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,51 @@
-import { Button, Typography, Input } from "./design-system";
+import { useState, useId } from "react";
+import { Button, Typography, Input, Label } from "./design-system";
 
 const App = () => {
+    const [value, setValue] = useState<string>("");
+    const [text, setText] = useState<string>("");
+
+    const handleOnChange = (value: string) => {
+        console.log(value);
+        setValue(value);
+    };
+
+    const handleOnChangeTextarea = (value: string) => {
+        setText(value);
+    };
     return (
         <div style={{ padding: "100px" }}>
             <Typography variant="h5">Hello</Typography>
-            <Button size="md" color="primary">
-                Test
-            </Button>
-            <Button size="lg" color="primary">
-                Test
-            </Button>
-            <Button size="sm" color="danger" shape="circle">
-                Welcome To TypeScript
-            </Button>
-            <form onSubmit={() => alert("submitted")}>
+
+            <form onSubmit={() => alert(`${value} ${text}`)} noValidate>
                 <Input
                     type="email"
                     placeholder="Email"
                     size="md"
                     shape="rounded"
                     labelText="Email"
-                />
-                <Input
-                    placeholder="First Name"
-                    size="md"
-                    shape="rounded"
-                    labelText="First Name"
-                />
-                <Input
-                    placeholder="Last Name"
-                    size="md"
-                    shape="rounded"
-                    labelText="Last Name"
-                />
-                <Input
-                    placeholder="About You"
-                    size="lg"
-                    type="textarea"
-                    shape="rounded"
-                    labelText="Tell us about yourself"
-                    hintMessage="This is for your password"
+                    onChange={handleOnChange}
+                    value={value}
                 />
 
-                <Button>Submit</Button>
+                <Input
+                    type="textarea"
+                    placeholder="About me"
+                    size="md"
+                    shape="rounded"
+                    labelText="Email"
+                    onChange={handleOnChangeTextarea}
+                    value={text}
+                />
+
+                <Button color="primary" shape="rounded">
+                    Submit
+                </Button>
             </form>
         </div>
     );
 };
+
+<button type="submit"></button>;
 
 export { App };
