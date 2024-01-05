82c651301ead23c46ce9b3cb1bd9e35df0e6d9e6
Tohir Kuliev <kuliev.tech@gmail.com>
2024-01-05 15:22:33 -0500

    set up basic routes

--- src/pages/admin-auth/reset-password/AdminResetPassword.tsx
+++ src/pages/admin-auth/reset-password/AdminResetPassword.tsx
@@ -3,6 +3,7 @@ import { PasswordWrapper } from "../../components";
 import { Input, Button } from "../../../design-system";
 import updatePassword from "../../../assets/images/update-password.svg";
 import styled from "styled-components";
+import { useSearchParams } from "react-router-dom";
 
 const Form = styled.form`
     width: 100%;
@@ -13,6 +14,8 @@ const Form = styled.form`
 const AdminResetPassword = () => {
     const [newPassword, setNewPassword] = useState<string>("");
     const [passwordConfirm, setPasswordConfirm] = useState<string>("");
+    const [searchParams] = useSearchParams();
+    const resetPasswordToken = searchParams.get("resetPasswordToken");
 
     const handleOnChangeNewPassword = (value: string) => {
         setNewPassword(value);
