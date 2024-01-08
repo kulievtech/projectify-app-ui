3c019cb1c28ba5f5d22ba9e1c691a4252bd5eb19
Tohir Kuliev <kuliev.tech@gmail.com>
2024-01-07 20:51:21 -0500

    move admin links to a seperate files

---
+++ src/pages/admin-platform/adminLinks.ts
@@ -0,0 +1,44 @@
+const adminLinks = [
+    {
+        title: "Menu",
+        links: [
+            {
+                linkText: "Project",
+                linkTo: "projects",
+                iconName: "projects"
+            },
+            {
+                linkText: "Stories",
+                linkTo: "stories",
+                iconName: "stories"
+            },
+            {
+                linkText: "Personal Tasks",
+                linkTo: "personal-tasks",
+                iconName: "tasks"
+            },
+            {
+                linkText: "Team Members",
+                linkTo: "team-members",
+                iconName: "members"
+            }
+        ]
+    },
+    {
+        title: "Settings",
+        links: [
+            {
+                linkText: "Settings",
+                linkTo: "settings",
+                iconName: "settings"
+            },
+            {
+                linkText: "Support",
+                linkTo: "support",
+                iconName: "support"
+            }
+        ]
+    }
+];
+
+export { adminLinks };
