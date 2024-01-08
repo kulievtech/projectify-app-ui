f3d6779f7f7a9de993c2f59156eb162513e8d16f
Tohir Kuliev <kuliev.tech@gmail.com>
2024-01-07 20:51:57 -0500

    create a team-member-platform

---
+++ src/pages/team-member-platform/TeamMemberPlatform.tsx
@@ -0,0 +1,28 @@
+import { Outlet } from "react-router-dom";
+import { AppContent, AppLayout, SideBarUser } from "../components";
+import user from "../../assets/images/user2.jpg";
+import { SideBar, SideBarLinks } from "../../design-system";
+import { teamMemberLinks } from "./teamMemberLinks";
+
+const TeamMemberPlatform = () => {
+    return (
+        <AppLayout>
+            <SideBar>
+                <SideBarUser
+                    details={{
+                        firstName: "Alex",
+                        lastName: "Williams",
+                        imageUrl: user,
+                        email: "example@gmail.com"
+                    }}
+                />
+                <SideBarLinks links={teamMemberLinks} />
+            </SideBar>
+            <AppContent>
+                <Outlet />
+            </AppContent>
+        </AppLayout>
+    );
+};
+
+export { TeamMemberPlatform };
---
+++ src/pages/team-member-platform/teamMemberLinks.ts
@@ -0,0 +1,39 @@
+const teamMemberLinks = [
+    {
+        title: "Menu",
+        links: [
+            {
+                linkText: "Project",
+                linkTo: "projects",
+                iconName: "projects"
+            },
+            {
+                linkText: "Stories",
+                linkTo: "stories",
+                iconName: "stories"
+            },
+            {
+                linkText: "Personal Tasks",
+                linkTo: "personal-tasks",
+                iconName: "tasks"
+            }
+        ]
+    },
+    {
+        title: "Settings",
+        links: [
+            {
+                linkText: "Settings",
+                linkTo: "settings",
+                iconName: "settings"
+            },
+            {
+                linkText: "Support",
+                linkTo: "support",
+                iconName: "support"
+            }
+        ]
+    }
+];
+
+export { teamMemberLinks };
