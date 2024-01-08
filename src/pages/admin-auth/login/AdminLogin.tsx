d47d2ccefa02476019678a8cb9f666b7ff7289e0
Tohir Kuliev <kuliev.tech@gmail.com>
2024-01-08 12:25:49 -0500

    add useRef to forms

--- src/pages/admin-auth/login/AdminLogin.tsx
+++ src/pages/admin-auth/login/AdminLogin.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import { Button, Input } from "../../../design-system";
 import { AuthWrapper } from "../../components";
 import styled from "styled-components";
@@ -20,6 +20,14 @@ const Form = styled.form`
 const AdminLogin = () => {
     const [email, setEmail] = useState<string>("");
     const [password, setPassword] = useState<string>("");
+
+    const focusRef = useRef<HTMLInputElement | HTMLTextAreaElement | null>(
+        null
+    );
+
+    useEffect(() => {
+        focusRef.current?.focus();
+    }, []);
 
     const handleOnChangeEmail = (value: string) => {
         setEmail(value);
@@ -45,6 +53,7 @@ const AdminLogin = () => {
                     shape="rounded"
                     size="lg"
                     className="login__email"
+                    inputRef={focusRef}
                 />
                 <Input
                     type="password"
