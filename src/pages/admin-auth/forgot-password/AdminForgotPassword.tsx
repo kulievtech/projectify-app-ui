1528d2524ec51f2d5b0052b2c4c51e204bfde93a
Tohir Kuliev <kuliev.tech@gmail.com>
2024-01-10 22:54:27 -0500

    implement back button link to go to Login

--- src/pages/admin-auth/forgot-password/AdminForgotPassword.tsx
+++ src/pages/admin-auth/forgot-password/AdminForgotPassword.tsx
@@ -1,16 +1,43 @@
 import { useState } from "react";
 import { PasswordWrapper } from "../../components";
 import toast from "react-hot-toast";
-import { Input, Button, Toaster } from "../../../design-system";
+import {
+    Input,
+    Button,
+    Toaster,
+    Icon,
+    Typography
+} from "../../../design-system";
 import forgotPassword from "../../../assets/illustrations/forgot-password.svg";
 import styled from "styled-components";
 import { useFocus } from "../../../custom-hooks/useFocus";
 import { admin } from "../../../api";
+import { Link } from "react-router-dom";
 
 const Form = styled.form`
     width: 100%;
     display: grid;
     gap: var(--space-20);
+`;
+
+const StyledBackArrowLink = styled(Link)`
+    width: max-content;
+    display: flex;
+    gap: var(--space-20);
+    margin: var(--space-50) 0 0 var(--space-50);
+    cursor: pointer;
+
+    .arrow__left-icon {
+        fill: var(--primary-500);
+
+        /* stroke: var(--primary-500);
+        stroke-width: 0.2rem;
+        stroke-linecap: round; */
+    }
+
+    .back-text {
+        color: var(--jaguar-900);
+    }
 `;
 
 const AdminForgotPassword = () => {
@@ -38,6 +65,16 @@ const AdminForgotPassword = () => {
 
     return (
         <>
+            <StyledBackArrowLink to={"../admin/login"}>
+                <Icon iconName="arrow-left" className="arrow__left-icon" />
+                <Typography
+                    variant="paragraphSM"
+                    weight="medium"
+                    className="back-text"
+                >
+                    Back
+                </Typography>
+            </StyledBackArrowLink>
             <PasswordWrapper
                 pageTitle="Forgot Password?"
                 imageUrl={forgotPassword}
