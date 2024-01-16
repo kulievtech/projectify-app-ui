20e6effcc3443264265842ede3d742cbfa15ee84
Tohir Kuliev <kuliev.tech@gmail.com>
2024-01-15 20:10:09 -0500

    add temporary protected routes to admin and team-member platform

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -17,59 +17,69 @@ import {
     TeamMemberForgotPassword
 } from "../pages";
 import { App } from "../App";
+import { ProtectedRoutes } from "./ProtectedRoutes";
 
 export const router = createBrowserRouter(
     createRoutesFromElements(
         <>
-            <Route path="/" element={<App />}></Route>
-            <Route path="/admin/sign-up" element={<AdminSignUp />}></Route>
-            <Route path="/admin/login" element={<AdminLogin />}></Route>
+            <Route path="/" element={<App />} />
+            <Route path="/admin/sign-up" element={<AdminSignUp />} />
+            <Route path="/admin/login" element={<AdminLogin />} />
             <Route
                 path="/admin/forgot-password"
                 element={<AdminForgotPassword />}
-            ></Route>
+            />
             <Route
                 path="/admin/reset-password"
                 element={<AdminResetPassword />}
-            ></Route>
+            />
             <Route
                 path="/team-member/create-password"
                 element={<TeamMemberCreatePassword />}
-            ></Route>
-            <Route
-                path="/team-member/login"
-                element={<TeamMemberLogin />}
-            ></Route>
+            />
+            <Route path="/team-member/login" element={<TeamMemberLogin />} />
             <Route
                 path="/team-member/forgot-password"
                 element={<TeamMemberForgotPassword />}
-            ></Route>
+            />
             <Route
                 path="/team-member/reset-password"
                 element={<TeamMemberResetPassword />}
-            ></Route>
-            <Route path="/admin/platform" element={<AdminPlatform />}>
-                <Route path="projects" element={<AdminProjects />} />
-                <Route path="stories" element={<h1>Admin Stories</h1>} />
-                <Route
-                    path="personal-tasks"
-                    element={<h1>Admin Personal Tasks</h1>}
-                />
-                <Route path="team-members" element={<h1>Admin Members</h1>} />
+            />
+
+            <Route element={<ProtectedRoutes userType="admin" />}>
+                <Route path="/admin/platform" element={<AdminPlatform />}>
+                    <Route path="projects" element={<AdminProjects />} />
+                    <Route path="stories" element={<h1>Admin Stories</h1>} />
+                    <Route
+                        path="personal-tasks"
+                        element={<h1>Admin Personal Tasks</h1>}
+                    />
+                    <Route
+                        path="team-members"
+                        element={<h1>Admin Members</h1>}
+                    />
+                </Route>
             </Route>
-            <Route
-                path="/team-member/platform"
-                element={<TeamMemberPlatform />}
-            >
+
+            <Route element={<ProtectedRoutes userType="team-member" />}>
                 <Route
-                    path="projects"
-                    element={<h1>Team Member Projects</h1>}
-                />
-                <Route path="stories" element={<h1>Team Member Stories</h1>} />
-                <Route
-                    path="personal-tasks"
-                    element={<h1>Team Member Personal Tasks</h1>}
-                />
+                    path="/team-member/platform"
+                    element={<TeamMemberPlatform />}
+                >
+                    <Route
+                        path="projects"
+                        element={<h1>Team Member Projects</h1>}
+                    />
+                    <Route
+                        path="stories"
+                        element={<h1>Team Member Stories</h1>}
+                    />
+                    <Route
+                        path="personal-tasks"
+                        element={<h1>Team Member Personal Tasks</h1>}
+                    />
+                </Route>
             </Route>
         </>
     )
