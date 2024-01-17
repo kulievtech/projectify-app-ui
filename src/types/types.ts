ca422a35d80eac2f0653d0f1f3d92c274ca342af
Tohir Kuliev <kuliev.tech@gmail.com>
2024-01-17 14:40:30 -0500

    add userType type

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,1 +1,14 @@
 export type UserRole = "admin" | "team-member";
+
+export interface UserType {
+    firstName: string;
+    lastName: string;
+    email: string;
+    id: string;
+    imageUrl?: string;
+    company: {
+        name: string;
+        position: string;
+    } | null;
+    role: UserRole;
+}
