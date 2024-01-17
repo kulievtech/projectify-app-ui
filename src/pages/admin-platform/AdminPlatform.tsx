d825e1a62c53ada4c342000560f60b586aa6e8fa
Tohir Kuliev <kuliev.tech@gmail.com>
2024-01-17 15:08:55 -0500

    implement get me in admin platform

--- src/pages/admin-platform/AdminPlatform.tsx
+++ src/pages/admin-platform/AdminPlatform.tsx
@@ -1,27 +1,55 @@
+import { useEffect } from "react";
 import { Outlet } from "react-router-dom";
+import { SideBar, SideBarLinks, Toaster } from "../../design-system";
 import { AppContent, AppLayout, SideBarUser } from "../components";
-import user from "../../assets/images/user.jpg";
-import { SideBar, SideBarLinks } from "../../design-system";
+import { admin } from "../../api";
+import toast from "react-hot-toast";
+import { Actions } from "../../store/actions";
+import { useStore } from "../../hooks";
 import { adminLinks } from "./adminLinks";
 
 const AdminPlatform = () => {
+    const {
+        state: { user },
+        dispatch
+    } = useStore();
+    useEffect(() => {
+        admin
+            .getMe()
+            .then((data): void => {
+                dispatch({
+                    type: Actions.INIT_USER,
+                    payload: data.data
+                });
+            })
+            .catch((error: Error) => {
+                toast.error(error.message);
+            });
+    }, []);
+
     return (
-        <AppLayout>
-            <SideBar>
-                <SideBarUser
-                    details={{
-                        firstName: "John",
-                        lastName: "Williams",
-                        imageUrl: user,
-                        email: "info@email.com"
-                    }}
-                />
-                <SideBarLinks links={adminLinks} loggedOutLink="/admin/login" />
-            </SideBar>
-            <AppContent>
-                <Outlet />
-            </AppContent>
-        </AppLayout>
+        <>
+            <AppLayout>
+                <SideBar>
+                    <SideBarUser
+                        details={{
+                            firstName: user?.firstName || "",
+                            lastName: user?.lastName || "",
+                            imageUrl: "",
+                            email: user?.email || ""
+                        }}
+                    />
+                    <SideBarLinks
+                        links={adminLinks}
+                        loggedOutLink="/admin/login"
+                    />
+                </SideBar>
+                <AppContent>
+                    <Outlet />
+                </AppContent>
+            </AppLayout>
+            <Toaster />
+        </>
     );
 };
 
