0442b08a6a77743e7a624c653d47a5b8451ec51e
Tohir Kuliev <kuliev.tech@gmail.com>
2024-01-21 17:13:30 -0500

    create a reusable badge

---
+++ src/design-system/Badge/Badge.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "./Badge.css";
+import { Icon } from "../Icon";
+import { Typography } from "../Typography";
+
+type BadgeProps = {
+    status: "todo" | "inprogress" | "done";
+    dueDate: string;
+};
+
+const Badge: React.FC<BadgeProps> = ({ dueDate, status }) => {
+    return (
+        <span className={`badge-wrapper ${status}-badge-wrapper`}>
+            <Icon
+                iconName={status === "done" ? "check-mark" : "flag"}
+                className={
+                    status === "done" ? `check-mark` : `flag ${status}-flag`
+                }
+            />
+            <Typography
+                variant="subtitleLG"
+                weight="semibold"
+                className={`${status}-due-date`}
+            >
+                {dueDate}
+            </Typography>
+        </span>
+    );
+};
+
+export { Badge };
