ecd5a9e0538e13f47abb943787e7e4409e01ca8c
Tohir Kuliev <kuliev.tech@gmail.com>
2024-02-12 15:55:37 -0500

    Make Select Scrollable

--- src/design-system/Select/Select.tsx
+++ src/design-system/Select/Select.tsx
@@ -1,5 +1,5 @@
 import { Option, OptionValue, SelectProps } from "./types";
-import { useEffect, useRef, useState } from "react";
+import { useRef } from "react";
 import { Label } from "../Label";
 import { Button } from "../Button";
 import "./Select.css";
