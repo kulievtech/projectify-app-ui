f8258ab9723873ad7f91fd4a8d7f6d3e23c05786
Tohir Kuliev <kuliev.tech@gmail.com>
2024-02-21 10:02:16 -0500

    Add filter option to projecs table

---
+++ src/pages/admin-platform/projects/ProjectsFilters.tsx
@@ -0,0 +1,57 @@
+import styled from "styled-components";
+import { Option, OptionValue, Select } from "../../../design-system";
+
+const FiltersBase = styled.section`
+    display: grid;
+    grid-template-columns: 20rem 1fr 20rem;
+    align-items: center;
+    margin-bottom: var(--space-20);
+    .filter-by-project {
+        grid-column: 1 / 2;
+    }
+    .filter-by-status {
+        grid-column: 3 / 4;
+    }
+`;
+
+const statusOptions = [
+    { label: "Active", value: "ACTIVE" },
+    { label: "Inactive", value: "INACTIVE" },
+    { label: "Deactivated", value: "DEACTIVATED" },
+    { label: "Default", value: "DEFAULT" }
+];
+
+type TeamMemberFiltersProps = {
+    status: OptionValue | undefined;
+    handleSetStatus: (value: Option) => void;
+};
+
+const ProjectFilters: React.FC<TeamMemberFiltersProps> = ({
+    status,
+    handleSetStatus
+}) => {
+    return (
+        <FiltersBase>
+            <Select
+                value=""
+                onSelect={() => {}}
+                options={[]}
+                shape="rounded"
+                size="md"
+                headerPlaceholder="By Project"
+                className="filter-by-project"
+            />
+            <Select
+                value={status}
+                onSelect={handleSetStatus}
+                options={statusOptions}
+                shape="rounded"
+                size="md"
+                headerPlaceholder="By Status"
+                className="filter-by-status"
+            />
+        </FiltersBase>
+    );
+};
+
+export { ProjectFilters };
