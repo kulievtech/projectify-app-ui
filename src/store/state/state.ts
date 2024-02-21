c065dbf9de64c23e6a372dc1b64dbe3b5eb24d93
Tohir Kuliev <kuliev.tech@gmail.com>
2024-02-21 09:58:04 -0500

    add state to projects

--- src/store/state/state.ts
+++ src/store/state/state.ts
@@ -1,19 +1,27 @@
-import { AdminUser, Task, TeamMember, TeamMemberUser } from "../../types";
+import {
+    AdminUser,
+    Project,
+    Task,
+    TeamMember,
+    TeamMemberUser
+} from "../../types";
 
 export type UserState = AdminUser | TeamMemberUser | null;
 export type TaskState = Task[];
 export type TeamMemberState = TeamMember[];
-
+export type ProjectState = Project[];
 export interface GlobalState {
     user: UserState;
     adminPersonalTasks: TaskState;
     teamMemberPersonalTasks: TaskState;
     teamMembers: TeamMemberState;
+    projects: ProjectState;
 }
 
 export const initialState: GlobalState = {
     user: null,
     adminPersonalTasks: [],
     teamMembers: [],
-    teamMemberPersonalTasks: []
+    teamMemberPersonalTasks: [],
+    projects: []
 };
