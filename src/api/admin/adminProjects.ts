742826a33b859cae70708b04042e5c945f8e6e71
Tohir Kuliev <kuliev.tech@gmail.com>
2024-02-21 10:00:30 -0500

    Hit admin projects create API

---
+++ src/api/admin/adminProjects.ts
@@ -0,0 +1,43 @@
+import { Project } from "../../types";
+
+type CreateInput = Omit<Project, "id" | "status">;
+
+type CreateInputResponse = {
+    data: Project;
+};
+
+class AdminProjectsService {
+    url: string;
+    constructor() {
+        this.url = `${
+            process.env.NODE_ENV === "development"
+                ? process.env.REACT_APP_PROJECTIFY_API_URL_LOCAL
+                : process.env.REACT_APP_PROJECTIFY_API_URL
+        }/projects`;
+    }
+
+    async create(input: CreateInput): Promise<CreateInputResponse> {
+        try {
+            const rawAuthToken = localStorage.getItem("authToken");
+            const authToken = rawAuthToken ? JSON.parse(rawAuthToken) : "";
+            const response = await fetch(`${this.url}`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    authorization: `Bearer ${authToken}`
+                },
+                body: JSON.stringify(input)
+            });
+            if (!response.ok) {
+                const data = await response.json();
+                throw new Error(data.message);
+            }
+
+            return response.json();
+        } catch (error) {
+            throw error;
+        }
+    }
+}
+
+export const adminProjectsService = new AdminProjectsService();
